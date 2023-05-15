b0446ede345e40f0c37d7caae4b3c3d94494149b
Ayas-HoloFair <126272427+Ayas-HoloFair@users.noreply.github.com>
2023-05-15 10:00:00 +0300

    Adjustments

--- upload.php
+++ upload.php
@@ -1,10 +1,11 @@
 <?php
 if ($_FILES['image']['error'] == 0) {
     $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES["image"]["name"]);
+    $file_name = preg_replace('/[^a-zA-Z0-9._-]/', '_', basename($_FILES["image"]["name"]));
+    $target_file = $target_dir . $file_name;
     
     if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
-        echo json_encode(['status' => 'success', 'file' => basename($_FILES["image"]["name"])]);
+        echo json_encode(['status' => 'success', 'file' => $file_name]);
     } else {
         echo json_encode(['status' => 'error', 'message' => 'Failed to upload']);
     }
